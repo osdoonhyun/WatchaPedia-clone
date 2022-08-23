e27ce4807f057032bada8bc9af1d7787fb167dde
osdoonhyun <skw1996@naver.com>
2022-08-24 03:41:13 +0900

    Home modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>WatchaPedia</h2>
-      </div>
+      <Header/>        
+      <Routes>
+        <Route path="/login" element={<login />} />
+        <Route path="/signUp" element={<signUp />} />
+      </Routes>
+      <h2>WatchaPedia 본문</h2>
     </div>
   );
+ 
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,52 @@
 import React from "react";
 import './Header.css';
+import SearchIcon from "@material-ui/icons/Search"
+import login from "./routes/login";
+import signUp from "./routes/signUp";
+import AppRouter from "./router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
+  const navigate = useNavigate();
   return (
-    <div className="header">
-      <img className='header_logo' src=''/>
-      <div className="header_nav">
-        <div className="header_menu">영화</div>
-        <div className="header_menu">TV</div>
-        <div className="header_menu">책</div>
-        <div className="header_menu">웹툰</div>
-      </div>
-      <div className="header_option">
-        <div className="header_search">
-          <input type='text' className='header_searchInput'/>
+    <>
+      <div className="header">
+
+        <div className="header_left">
+          <img 
+            className='header_logo' 
+            src='img/watchapedia_logo.png'
+            alt='watchapedia_logo'
+          />
+          <div className="header_nav">
+            <span className="header_menu">영화</span>
+            <span className="header_menu">TV</span>
+            <span className="header_menu">책</span>
+            <span className="header_menu">웹툰</span>
+          </div>
         </div>
-        <div className="header_logIn">로그인</div>
-        <div className="header_signUp">회원가입</div>
-      </div>  
-    </div>
+
+        <div className="header_right">
+          <div className="header_option">
+            <div className="header_search">
+              <input 
+                type='text' 
+                placeholder='콘텐츠,인물,컬렉션, 유저를 검색해보세요.'            
+                className='header_searchInput'
+              />
+              <SearchIcon className="header_searchIcon"/>
+            </div>
+            <Router>
+              <button className="header_login" onClick={() => navigate("/login")}>로그인</button>
+              <button className="header_signUp" onClick={() => navigate("/signUp")}>회원가입</button>
+          </Router>
+          </div>  
+        </div>
+
+      </div>
+    </>
   );
 }
 
