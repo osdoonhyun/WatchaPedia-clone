57f06a79d97d4faf229b7e71af6838d877770e25
osdoonhyun <skw1996@naver.com>
2022-08-25 23:51:20 +0900

    create Login SignUp button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Header from './Header';
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Header/>        
-      <Routes>
-        <Route path="/login" element={<login />} />
-        <Route path="/signUp" element={<signUp />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+      </Router>       
       <h2>WatchaPedia 본문</h2>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,10 @@
 import React from "react";
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search"
-import login from "./routes/login";
-import signUp from "./routes/signUp";
-import AppRouter from "./router";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 function Header() {
-  const navigate = useNavigate();
+
   return (
     <>
       <div className="header">
@@ -38,11 +33,9 @@ function Header() {
               />
               <SearchIcon className="header_searchIcon"/>
             </div>
-            <Router>
-              <button className="header_login" onClick={() => navigate("/login")}>로그인</button>
-              <button className="header_signUp" onClick={() => navigate("/signUp")}>회원가입</button>
-          </Router>
-          </div>  
+          </div> 
+          <Link to="./Login">Login</Link>
+          <Link to="./SignUp">SignUp</Link>
         </div>
 
       </div>
